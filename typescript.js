8feed4176fdac221ae8bef662531e99ef60cd58c
sulmeee <poorooria@naver.com>
2022-06-28 00:09:52 +0900

    quiz

--- typescript.js
+++ typescript.js
@@ -1,38 +1,40 @@
-//// #2.1 Implicit Types vs Explicit Types 
-// 타입 시스템에 대해 알아보자
-// 자바스크립트에서는 변수를 생성하기만 하고 타입을 지정하지 않았다
-// Java, C# 은 타입을 명시해야한다
+// Quiz
 
-// 데이터와 변수의 타입을 명시적으로 정의할 수도 있고
-// 자바스크립트처럼 변수만 생성하고 넘어가도 된다
-// 타입을 추론해준다
+// QUESTION 1 OF 7
+// JavaScript is a type safe langauge.
+// 자바스크립트는 타입이 안전한 언어인가요?
+// No
 
-let a = "hello"
-// a의 타입을 추론 >>> 문자열
+// QUESTION 2 OF 7
+// What happens if we run this code in JS:
+// ["hello"]+{hi:1}
+// It will run
 
-a = "bye"
-// string > string
+// QUESTION 3 OF 7
+// What is a runtime error?
+// 런타임 에러가 무엇인가요?
+// An error that happens while the code runs
+// 코드가 실행되는 동안 발생하는 오류
 
-// a = 1
-// 안된다 
+// QUESTION 4 OF 7
+// What is Typescript compiled to?
+// JavaScript Code
 
+// QUESTION 5 OF 7
+// Is this valid Typescript code?
+// 유효한 타입스크립트 코드인가요?
+// const kimchi = {
+//     맛있어: true
+// }
+// Yes
 
-// 타입을 자세하게 알려주기
-// let b : boolean = "x"
-// Type 'string' is not assignable to type 'boolean'.
+// QUESTION 6 OF 7
+// We always have to specify a type for our variables in Typescript.
+// 항상 Typescript에서 변수의 유형을 지정해야 합니다
+// No, Typescript can infer it sometimes
+// 아니요, Typescript는 때때로 그것을 추론할 수 있습니다.
 
-// let b: boolean = false
-let b = false
-// : boolean <<< 타입스크립트 문법
-
-// let c = [1,2,3]
-// // number[]
-
-// c.push("1")
-// Argument of type 'string' is not assignable to parameter of type 'number'.
-
-// 만약 빈 배열을 선언했다면 명시적 표현을 사용하자
-let c = []
-// let c: number[] = []
-// 이럴 땐 명시적 표현이 유용하지만 명시적 표현은 최소화하는게 좋다
-// 타입스크립트가 추론하게 시키도록 하자
+// QUESTION 7 OF 7
+// How do we set the type of a variable explicitly in TS?
+// TS에서 명시적으로 변수의 유형을 설정하는 방법은 무엇입니까?
+// const x : string = "hi"
