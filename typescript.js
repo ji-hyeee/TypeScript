5d177f906f48e8a35074985a990594ac38205ea8
sulmeee <poorooria@naver.com>
2022-06-28 23:41:22 +0900

    Types of TS part Two

--- typescript.js
+++ typescript.js
@@ -1,104 +1,60 @@
-// #2.2 Types of TS part One
-// basic type
-let a: number = 1;
-let b: string = 'hye';
-let c: boolean = true;
-let d: boolean[] = [true];
-
-// optional type
-const player = {
-    name: 'hye'
-}
-
-player.name = 1 // 오류
-
-// 타입스크립트와 소통하기
-// 객체가 있거나 없거나하게 만들고 싶어요
-
-const player: object = {
-    name: 'hye'
-}
-
-// player.name
-// Property 'name' does not exist on type 'object'.
-// 타입을 적어줘도 object 에는 name 요소가 없다고 오류를 보낸다
-
-// object의 타입을 정의하자
-// const player : {} = {}
-
-const player: {
-    name: string, // 객체값의 타입
-    age?: number
-} = {
-    name: "hye" // 실제 데이터
-}
-
-// age를 적거나 적지 않거나 하고 싶어요 (선택적)
-// 물음표를 적어주자 !!!
-// age?:number // number | undefined
-
-// if(player.age < 10){
-
-// }
-// Object is possibly 'undefined'.
-
-if (player.age && player.age < 10) {
-
-}
-
-// 만약 player를 엄청 많이 만든다면 ?
-// 반복적으로 타입을 적고 값을 써야한다
-// Alias(별칭) 타입을 생성하자
-
+// #2.3 Types of TS part Two
+type Age = number;
+type Name = string;
 type Player = {
-    name: string,
-    age?: number
+    readonly name: Name,
+    age?: Age
 }
-
-const hye: Player = {
-    name: "hye"
-}
-
-// 타입재사용 !
-
-// 타입마다 별칭을 사용해줄수도 있다
-// type Age = number;
-// type Player = {
-//     name:string,
-//     age?:Age
-// }
-
-// 함수의 return 값의 타입을 지정하는 방법을 알아보자
-// 지금까지 변수에 타입을 할당해줬었다
-// 함수가 반환하는 타입이 무엇인지 알면 자동완성, 보호장치가 더 많아진다
-
-function playerMaker(name: string) { // 문자열 인자를 받을것이다
-    return {
-        name: name
-        // name / 같은 이름을 가지면 name 만 써줘도 된다
-    }
-}
-
-// 함수가 Player 타입을 리턴하게 만들고 싶다면?
-// const jihye = playerMaker("hye") // 문자열 인자를 적어주세용
-// jihye.age = 24 // 오류
-
-// 타입스크립트에게 함수가 리턴하는 타입을 어떻게 말해줘야할까
-// 기존에 썼던 것 그대로 해주자
-
-function playerMaker(name: string): Player { // <<< Player 타입 적어주기
-    return {
-        name: name
-    }
-}
-
-const jihye = playerMaker("hye")
-jihye.age = 24
-
-// 변수 뒤에 타입
-// 인자 뒤에 타입
-// 함수 뒤에 타입
-
-// 화살표함수
-// const playerMaker = (name:string) => ({name}) // 오류 타입정해주기 !
 const playerMaker = (name: string): Player => ({ name })
+const hye = playerMaker("hye")
+hye.age = 24
+
+// readonly 속성을 타입에 추가해보자
+// 요소들을 읽기전용으로 만들기
+
+hye.name = "jihye"
+// Cannot assign to 'name' because it is a read-only property.
+
+const numbers: readonly number[] = [1, 2, 3, 4]
+numbers.push(1)
+// Identifier expected.
+
+const names: readonly string[] = ['1', '2']
+names.push("3")
+
+// 불변성을 가지게 된다
+
+// tuple
+// 배열을 생성한다
+// 최소한의 길이를 가지고 특정 위치에 특정 타입이 있어야 한다
+// ["hye", 24, true]
+const player: [string, number, boolean] = ["hye", 24, true]
+// 언제 써야할까
+// 항상 정해진 갯수의 요소를 가져야 하는 배열에 지정
+// 원하는 순서에 맞는 타입을 가져야할 때
+// API가 배열을 줄 수도 있다 / 그럴 때 사용
+
+player[0] = 1
+// Type 'number' is not assignable to type 'string'. / 오류
+
+// 읽기전용으로 변경도 가능
+// const player: readonly [string, number, boolean] = ["hye", 24, true]
+
+let a: undefined = undefined
+let a: null = null
+// 선택적 타입에는 undefined 가 들어갈 수 있다
+
+// any
+// 비어있는 값
+
+let a: [] // any[]
+// 타입스크립트로부터 빠져나오고 싶을 때 사용하는 타입
+
+// any 사용을 막기 위해 추가하는 몇가지 규칙이 있다
+// 되도록 사용하지 않는 것이 좋다 / 보호장치를 잃어버림
+
+const a: any[] = [1, 2, 3, 4]
+const b: any = true
+a + b // 좋지 않습니다
+
+// 가끔 any 를 써야할 때가 있다 그럴땐쓰세요 ~
