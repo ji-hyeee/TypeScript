8ea4facca2a24daf66b95816b26ca0b0c6741e4f
sulmeee <poorooria@naver.com>
2022-06-29 00:02:22 +0900

    quiz

--- typescript.js
+++ typescript.js
@@ -1,59 +1,64 @@
-// #2.4 Types of TS part Three
-// 새로운 타입을 알아보자
-// 타입스크립트의 type checker와 소통한다
+// Quiz
 
-// 타입을 모르는 변수를 타입스트립트에게 알려주려면?
-// unknown
-
-let a: unknown;
-// 변수의 타입을 확인하는 방식으로 보호받는다
-
-// let b = a + 1
-// Object is of type 'unknown'.
-
-if (typeof a === 'number') { // 만약 a의 타입이 숫자라면 ~
-    let b = a + 1
-}
-
-if (typeof a === 'string') {
-    let b = a.toUpperCase();
-}
+// QUESTION 1 OF 11
+// What is the syntax to type an array of booleans?
+// : boolean[]
 
 
-// void
-// 비어있는 것
-// 아무것도 리턴하지 않는 함수를 대상으로 사용
+// QUESTION 2 OF 11
+// In this code:
+// const player: {
+//     name?:string,
+//     age:number
+// } = {
+//     name:"nico",
+//     age:44
+// }
+// `name` is optional. `age` is required
 
-function hello() { // hello(): void
-    console.log('x')
-}
 
-const b = hello();
-b.toUpperCase()
-// Property 'toUpperCase' does not exist on type 'void'. / 오류
+// QUESTION 3 OF 11
+// For what do we use Type Aliases?
+// To create a new name for a type
 
 
-// never
-// 함수가 절대 리턴하지 않을 때 사용
-// 함수에서 exception(예외)가 발생했을 때
+// QUESTION 4 OF 11
+// What is the syntax to say that a function returns an array of strings?
+// function name(): string[]
 
-function hi(): never {
-    return "X"
-}
-// Type 'string' is not assignable to type 'never'. / 오류
 
-function hye(): never {
-    throw new Error("XXX") // 오류 발생 시키기
-}
+// QUESTION 5 OF 11
+// Does the readonly from Typescript compile to JavaScript?
+// No
 
-// never 는 타입이 두가지일수도 있는 상황에 발생할 수 있다
 
-function jihye(name: string | number) {
-    if (typeof name === "string") {
-        name // string
-    } else if (typeof name === "number") {
-        name // number
-    } else {
-        name // never / 절대 실행되지 않는다
-    }
-}
+// QUESTION 6 OF 11
+// Why do we use Tuples for?
+// To specify an array with a min. length and type positions
+// 개체에 유형을 추가하려면 최소값으로 배열을 지정하려면 길이 및 유형 위치
+
+
+// QUESTION 7 OF 11
+// We should try to use any as much as possible.
+// No
+
+
+// QUESTION 8 OF 11
+// What happens when we use any?
+// We escape the TS world
+
+
+// QUESTION 9 OF 11
+// What do we have to do before using a variable of type unknown?
+// We have to first check with typeof
+// 먼저 typeof를 확인해야 합니다.
+
+
+// QUESTION 10 OF 11
+// When do we use void?
+// When a function does not return anything
+
+
+// QUESTION 11 OF 11
+// When do we use never?
+// When a path of code should never run
