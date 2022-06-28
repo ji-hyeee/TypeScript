a9839d3758e4e81c3588944ffbb1e95f30a8bc0d
sulmeee <poorooria@naver.com>
2022-06-28 22:39:32 +0900

    Types of TS part One

--- typescript.js
+++ typescript.js
@@ -1,40 +1,104 @@
-// Quiz
+// #2.2 Types of TS part One
+// basic type
+let a: number = 1;
+let b: string = 'hye';
+let c: boolean = true;
+let d: boolean[] = [true];
 
-// QUESTION 1 OF 7
-// JavaScript is a type safe langauge.
-// 자바스크립트는 타입이 안전한 언어인가요?
-// No
+// optional type
+const player = {
+    name: 'hye'
+}
 
-// QUESTION 2 OF 7
-// What happens if we run this code in JS:
-// ["hello"]+{hi:1}
-// It will run
+player.name = 1 // 오류
 
-// QUESTION 3 OF 7
-// What is a runtime error?
-// 런타임 에러가 무엇인가요?
-// An error that happens while the code runs
-// 코드가 실행되는 동안 발생하는 오류
+// 타입스크립트와 소통하기
+// 객체가 있거나 없거나하게 만들고 싶어요
 
-// QUESTION 4 OF 7
-// What is Typescript compiled to?
-// JavaScript Code
+const player: object = {
+    name: 'hye'
+}
 
-// QUESTION 5 OF 7
-// Is this valid Typescript code?
-// 유효한 타입스크립트 코드인가요?
-// const kimchi = {
-//     맛있어: true
+// player.name
+// Property 'name' does not exist on type 'object'.
+// 타입을 적어줘도 object 에는 name 요소가 없다고 오류를 보낸다
+
+// object의 타입을 정의하자
+// const player : {} = {}
+
+const player: {
+    name: string, // 객체값의 타입
+    age?: number
+} = {
+    name: "hye" // 실제 데이터
+}
+
+// age를 적거나 적지 않거나 하고 싶어요 (선택적)
+// 물음표를 적어주자 !!!
+// age?:number // number | undefined
+
+// if(player.age < 10){
+
 // }
-// Yes
+// Object is possibly 'undefined'.
 
-// QUESTION 6 OF 7
-// We always have to specify a type for our variables in Typescript.
-// 항상 Typescript에서 변수의 유형을 지정해야 합니다
-// No, Typescript can infer it sometimes
-// 아니요, Typescript는 때때로 그것을 추론할 수 있습니다.
+if (player.age && player.age < 10) {
 
-// QUESTION 7 OF 7
-// How do we set the type of a variable explicitly in TS?
-// TS에서 명시적으로 변수의 유형을 설정하는 방법은 무엇입니까?
-// const x : string = "hi"
+}
+
+// 만약 player를 엄청 많이 만든다면 ?
+// 반복적으로 타입을 적고 값을 써야한다
+// Alias(별칭) 타입을 생성하자
+
+type Player = {
+    name: string,
+    age?: number
+}
+
+const hye: Player = {
+    name: "hye"
+}
+
+// 타입재사용 !
+
+// 타입마다 별칭을 사용해줄수도 있다
+// type Age = number;
+// type Player = {
+//     name:string,
+//     age?:Age
+// }
+
+// 함수의 return 값의 타입을 지정하는 방법을 알아보자
+// 지금까지 변수에 타입을 할당해줬었다
+// 함수가 반환하는 타입이 무엇인지 알면 자동완성, 보호장치가 더 많아진다
+
+function playerMaker(name: string) { // 문자열 인자를 받을것이다
+    return {
+        name: name
+        // name / 같은 이름을 가지면 name 만 써줘도 된다
+    }
+}
+
+// 함수가 Player 타입을 리턴하게 만들고 싶다면?
+// const jihye = playerMaker("hye") // 문자열 인자를 적어주세용
+// jihye.age = 24 // 오류
+
+// 타입스크립트에게 함수가 리턴하는 타입을 어떻게 말해줘야할까
+// 기존에 썼던 것 그대로 해주자
+
+function playerMaker(name: string): Player { // <<< Player 타입 적어주기
+    return {
+        name: name
+    }
+}
+
+const jihye = playerMaker("hye")
+jihye.age = 24
+
+// 변수 뒤에 타입
+// 인자 뒤에 타입
+// 함수 뒤에 타입
+
+// 화살표함수
+// const playerMaker = (name:string) => ({name}) // 오류 타입정해주기 !
+const playerMaker = (name: string): Player => ({ name })
