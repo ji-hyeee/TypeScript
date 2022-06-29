b9c4ef064fda43e6de80063c147123b015117a8e
sulmeee <poorooria@naver.com>
2022-06-29 22:59:21 +0900

    learn Call Signatures

--- typescript.js
+++ typescript.js
@@ -1,64 +1,29 @@
-// Quiz
+////  #3.0 Call Signatures
+// 함수!
+// 다형성(polymorphism)
+// 오버로딩(overloading)
+// 제네릭(generics)
 
-// QUESTION 1 OF 11
-// What is the syntax to type an array of booleans?
-// : boolean[]
+function add(a: number, b: number) {
+    return a + b
+}
+// 타입스크립트는 any 타입을 좋아하지 않는다
+// 타입을 지정해주자
 
+// 화살표 함수
+const add = (a: number, b: number) => a + b
 
-// QUESTION 2 OF 11
-// In this code:
-// const player: {
-//     name?:string,
-//     age:number
-// } = {
-//     name:"nico",
-//     age:44
-// }
-// `name` is optional. `age` is required
+//// Call Signatures
+// 인자에 타입을 작성하지 않고 싶어
+// add 함수만의 타입을 만들고 싶어 >>> Call Signatures
+// Call Signatures는 함수 위에 마우스를 올렸을 때 보게 되는 것
+// 함수를 어떻게 호출(타입)해야하는 것인지 알려준다
 
+// 맹글어보자
+type Add = (a: number, b: number) => number;
 
-// QUESTION 3 OF 11
-// For what do we use Type Aliases?
-// To create a new name for a type
+const add: Add = (a, b) => a + b
 
-
-// QUESTION 4 OF 11
-// What is the syntax to say that a function returns an array of strings?
-// function name(): string[]
-
-
-// QUESTION 5 OF 11
-// Does the readonly from Typescript compile to JavaScript?
-// No
-
-
-// QUESTION 6 OF 11
-// Why do we use Tuples for?
-// To specify an array with a min. length and type positions
-// 개체에 유형을 추가하려면 최소값으로 배열을 지정하려면 길이 및 유형 위치
-
-
-// QUESTION 7 OF 11
-// We should try to use any as much as possible.
-// No
-
-
-// QUESTION 8 OF 11
-// What happens when we use any?
-// We escape the TS world
-
-
-// QUESTION 9 OF 11
-// What do we have to do before using a variable of type unknown?
-// We have to first check with typeof
-// 먼저 typeof를 확인해야 합니다.
-
-
-// QUESTION 10 OF 11
-// When do we use void?
-// When a function does not return anything
-
-
-// QUESTION 11 OF 11
-// When do we use never?
-// When a path of code should never run
+// 장점
+// 타입을 만들 수 있고, 함수가 어떻게 작동하는지 서술해놓을 수 있다
+// 타입을 생각하도록 해준다 / 타입을 설명하고 코드를 구현함
